7777f8ed5c42bcb853d35b0271dabec701036b77
JOAOZINHOMOTOROLA <abreu.4999@aluno.pr.senac.br>
2025-02-26 12:20:45 -0300

    commit40

--- Src/scripts/main.js
+++ Src/scripts/main.js
@@ -1,1 +1,9 @@
+document.getElementById('form-sorteador').addEventListener('submit', event => {
 
+    event.preventDefault();
+    let numMax = parseInt(document.getElementById('numero-maximo').value);
+    let numAle = Math.floor(Math.random() * numMax) + 1;
+    
+    document.getElementById('numero-sorteado').innerText = numAle;
+    document.querySelector('.resultado').style.display = 'block';
+})
